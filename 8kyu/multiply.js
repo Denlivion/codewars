3f066649c98365fc8981c4b2e4d24dc753e9651d
Denlivion <dinegromoff@gmail.com>
2026-02-16 11:13:43 +0300

    loop-statement.js

---
+++ 8kyu/multiply.js
@@ -0,0 +1,11 @@
+// DESCRIPTION
+// This code does not execute properly. Try to figure out why.
+
+// SOLUTION
+let action;
+function multiply(a, b){
+    action = (a * b);
+    // console.log(action)
+    return (action);
+}
+multiply(2,5)
