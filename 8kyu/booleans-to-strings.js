8c796a889ec776eda7833aeb52069e0792630258
Denlivion <dinegromoff@gmail.com>
2026-02-17 09:08:14 +0300

    return-negative.js

---
+++ 8kyu/booleans-to-strings.js
@@ -0,0 +1,11 @@
+//👀DESCRIPTION
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+//👨‍💻SOLUTION
+let iSstring
+function boolToWord( bool ){
+    isString = ((bool===true)?'Yes':'No');
+    return(isString)
+}
+boolToWord(true)
