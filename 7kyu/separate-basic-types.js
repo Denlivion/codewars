e3c2bd1768165435cac19857048a92520cac1bb6
Denlivion <dinegromoff@gmail.com>
2026-04-15 22:52:06 +0300

    separate-basic-types-begining

---
+++ 7kyu/separate-basic-types.js
@@ -0,0 +1,23 @@
+//👀DESCRIPTION:
+// Given: a sequence of different type of values (number, string, boolean).
+// You should return an object with a separate properties for each of types presented in input.
+// Each property should contain an array of corresponding values.
+//
+//  _keep order of values like in input array
+//  _if type is not presented in input, no corresponding property are expected
+
+
+//🛷EXAMPLES:
+//['a', 1, 2, false, 'b']
+// -->
+//          {
+//              number: [1, 2],
+//              string: ['a', 'b'],
+//              boolean: [false]
+//          }
+
+//👨‍💻SOLUTION:
+
+function separateTypes(input) {
+
+}
